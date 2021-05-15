ea61815e8fbc8a7203ba32f55b8dee152707bc5a
Hsiao-Ting Yu <sst.dreams@gmail.com>
2021-05-15 12:19:17 +0800

    fix: use stringify only

--- dump.ts
+++ dump.ts
@@ -1,7 +1,6 @@
 import { promises } from "fs"
 import Airtable from "airtable"
 import { FeatureCollection } from "geojson"
-import { format } from "prettier"
 
 const base = new Airtable().base(process.env.AIRTABLE_BASE_ID ?? "")
 const records = await base("data").select({ view: "Grid view" }).all()
@@ -19,4 +18,4 @@ const result: FeatureCollection = {
     }
   }),
 }
-await promises.writeFile("result.geojson", format(JSON.stringify(result)))
+await promises.writeFile("result.geojson", JSON.stringify(result, null, 2))
